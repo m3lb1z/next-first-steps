f2c0a9a3c688d0f72bfa1de349d3c4d81ba26ced
Elvis M. <u201112959@upc.edu.pe>
2024-09-11 11:40:22 -0500

    feat: replace anchor tags with next/link in navbar component

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,16 +1,26 @@
-import React from 'react'
+import Link from 'next/link';
+// import React from 'react'
 
-export const Navbar = () => {
+// const temporalAsync = () => {
+//   return new Promise<void>((resolve) => {
+//     setTimeout(() => {
+//       resolve();
+//     }, 2000);
+//   });
+// };
 
+export const Navbar = async() => {
+
+  // await temporalAsync();
   // console.log('Navbar creado');
   
   return (
     <nav className='flex bg-blue-800 bg-opacity-30 p-2 m-2 rounded'>
         <span>Home</span>
         <div className='flex flex-1'></div>
-        <a className='mr-2' href='/about'>About</a>
-        <a className='mr-2' href='/pricing'>Pricing</a>
-        <a className='mr-2' href='/contact'>Contact</a>
+        <Link className='mr-2' href='/about'>About</Link>
+        <Link className='mr-2' href='/pricing'>Pricing</Link>
+        <Link className='mr-2' href='/contact'>Contact</Link>
     </nav>
   )
 }
